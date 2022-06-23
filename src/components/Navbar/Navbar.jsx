b2075ce2958123dc06a1532fba763e2e0535bd66
pooya <pooya13vm@gmail.com>
2022-06-23 23:13:14 +0300

    navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,53 @@
 import "./Navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search ..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon />
+            <span>English</span>
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
+          </div>
+          <div className="item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqEUd4DM4HcfROPWCX3NELmAT352vWREQbBZpAyZO0FA&s"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import "./Home.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="home-container">container</div>
+      <div className="home-container">
+        <Navbar />
+        <Widget />
+        <Widget />
+        <Widget />
+        <Widget />
+      </div>
     </div>
   );
 };
